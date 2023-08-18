69b3aa80fcdee490779b5a2e832833678710b048
trobonox <57040351+trobonox@users.noreply.github.com>
2023-08-18 14:05:09 +0300

    styles: use new color for main page subtext

--- packages/client/src/pages/index.tsx
+++ packages/client/src/pages/index.tsx
@@ -4,7 +4,7 @@ const Home: Component = () => {
     return (
         <div class="flex flex-col w-full h-full items-center pt-48"> 
             <h1 class=" text-5xl font-extrabold underline-accent underline underline-wavy mb-2">Does it Bun?</h1>
-            <h2 class="text-lg text-subtle">Easily check if your package is compatible with Bun.</h2>
+            <h2 class="text-lg text-subtext">Easily check if your package is compatible with Bun.</h2>
             <input class="mt-8 rounded-md px-3 py-2 h-12 w-xl bg-black-800" placeholder="Enter a package name..."></input>
         </div>
     );
