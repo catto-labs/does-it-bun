22b586d763eace539b341eb3d5668297ececc2e9
trobonox <57040351+trobonox@users.noreply.github.com>
2023-08-28 14:37:38 +0200

    feat: create testing table

--- packages/server/src/database/index.ts
+++ packages/server/src/database/index.ts
@@ -7,5 +7,14 @@ import { Database } from "bun:sqlite";
 const sqlite = new Database(SQLITE_DB_PATH, { create: true });
 const database = drizzle(sqlite);
 
+sqlite.run(`
+  CREATE TABLE IF NOT EXISTS package_compatibility (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    version TEXT NOT NULL,
+    compatible INTEGER NOT NULL
+  )
+`);
+
 export default database;
 export * from "./schemas";
