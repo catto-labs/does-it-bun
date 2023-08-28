2e2f184bf056477a40bc5377b20c40b553c55ee0
trobonox <57040351+trobonox@users.noreply.github.com>
2023-08-28 13:22:34 +0200

    feat: WIP try caching compatibility results

--- packages/server/src/database/schemas/packages.ts
+++ packages/server/src/database/schemas/packages.ts
@@ -6,3 +6,10 @@ export const packages = sqliteTable('packages', {
     version: text('version')
   }
 );
+
+export const packageCompatibility = sqliteTable('package_compatibility', {
+  id: integer('id').primaryKey(),
+  name: text('name'),
+  version: text('version'),
+  compatible: integer('compatible')
+});
--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -6,10 +6,85 @@ import extractTarball from "@/tarball/extract";
 import { scanFile } from "@/scan";
 import NPM from "@/npm";
 
-import database, { packages } from "@/database";
+import database, { packageCompatibility } from "@/database";
+import { and, eq } from "drizzle-orm/sql";
+
 
 const app = new Elysia()
   .use(cors())
+  .get("/is_package_compatible", async ({ query }) => {
+    const npm = new NPM(query.registry);
+    const pkg = await npm.package(query.name);
+    const version = query.version ?? pkg.tags["latest"];
+
+    // Check if the compatibility data is already cached
+    const cachedCompatibility = await database.select().from(packageCompatibility)
+        .where(and(eq(packageCompatibility.name, query.name), eq(packageCompatibility.version, version)));
+
+    if (cachedCompatibility) {
+        return {
+            success: true,
+            compatible: Boolean(cachedCompatibility[0].compatible)
+        };
+    }
+
+    // If not cached, perform the compatibility check
+    const pkg_data = pkg.getVersion(version);
+    const tarball = await downloadTarballAsReadable(pkg_data.tarball);
+    const files = await extractTarball(tarball);
+
+    const checked_files: Array<ReturnType<typeof scanFile>> = [];
+    for (const file of files) {
+      checked_files.push(scanFile(file));
+    }
+
+    let compatible = true;
+    for (const file of checked_files) {
+      if (file.ignored) continue;
+      
+      if (file.issues.length > 0) {
+        compatible = false;
+        break;
+      }
+    }
+
+    // Cache the compatibility result
+    await database.insert(packageCompatibility).values({
+        name: query.name,
+        version: version,
+        compatible: compatible ? 1 : 0
+    })
+
+    return {
+        success: true,
+        compatible: compatible
+    };
+  }, {
+    error({ code, error, set }) {
+      console.log(error);
+      if (code === "VALIDATION") {
+        set.status = error.status;
+        return {
+          success: false,
+          message: error.message
+        }
+      }
+
+      set.status = 500;
+      return {
+        success: false,
+        message: "Unknown error happened server side.",
+        debug: error
+      }
+    },
+    query: t.Object({
+      name: t.String({
+        error: "Needs name query parameter",
+      }),
+      version: t.Optional(t.String()),
+      registry: t.Optional(t.String())
+    })
+  })
   .get("/scan", async ({ query }) => {
     const npm = new NPM(query.registry);
     
@@ -25,9 +100,20 @@ const app = new Elysia()
       checked_files.push(scanFile(file));
     }
 
+    let compatible = true;
+    for (const file of checked_files) {
+      if (file.ignored) continue;
+      
+      if (file.issues.length > 0) {
+        compatible = false;
+        break;
+      }
+    }
+
     return {
       success: true,
-      data: checked_files
+      data: checked_files,
+      compatible: compatible
     }
   }, {
     error({ code, error, set }) {
@@ -54,7 +140,6 @@ const app = new Elysia()
       registry: t.Optional(t.String())
     })
   })
-  
   .get("/search", async ({ query }) => {
     const npm = new NPM(query.registry);
 
@@ -71,8 +156,21 @@ const app = new Elysia()
       data: data
     }
   }, {
-    error(err) {
-      // error handling
+    error({ code, error, set }) {
+      if (code === "VALIDATION") {
+        set.status = error.status;
+        return {
+          success: false,
+          message: error.message
+        }
+      }
+
+      set.status = 500;
+      return {
+        success: false,
+        message: "Unknown error happened server side.",
+        debug: error
+      }
     },
     query: t.Object({
       name: t.String({
